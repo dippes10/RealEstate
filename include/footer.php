a4fa2fa04d8e35ff9d51da4b3f08c3fa0f41d321
Dipes Sanjel <dippes10@gmail.com>
2023-03-19 21:26:37 +0545

    initial

---
+++ include/footer.php
@@ -0,0 +1,65 @@
+<footer class="full-row bg-secondary p-0">
+            <div class="container">
+                <div  class="row">
+                    <div class="col-lg-12">
+                        <div class="divider py-40">
+                            <div class="row">
+                                <div class="col-md-12 col-lg-4">
+                                    <div class="footer-widget mb-4">
+                                        <div class="footer-logo mb-4"> <a href="#"><img class="logo-bottom" src="images/logo/restatelg_white.png" alt="image"></a> </div>
+                                        <p class="pb-20 text-white">At Home Link Real Estate, we are passionate about helping our clients achieve their real estate goals. Our team of experienced and dedicated professionals is committed to providing the highest level of service and expertise to our clients.</p>
+										
+                                        </div>
+                                </div>
+                                <div class="col-md-12 col-lg-8">
+                                    <div class="row">
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Help & Support</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="#" class="text-white">Forum</a></li>
+                                                    <li><a href="#" class="text-white">Terms and Condition</a></li>
+                                                    <li><a href="#" class="text-white">Frequenlty Asked Question</a></li>
+                                                    <li><a href="contact.php" class="text-white">Contact</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Quick Links</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="about.php" class="text-white">About Us</a></li>
+                                                    <li><a href="#" class="text-white">Featured Property</a></li>
+                                                    <li><a href="#" class="text-white">Submit Property</a></li>
+                                                    <li><a href="agent.php" class="text-white">Our Agents</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Contact Us</h4>
+                                                <ul class="text-white">
+                                                    <li class="hover-text-primary"><i class="fas fa-map-marker-alt text-white mr-2 font-13 mt-1"></i>9 Gothatar, Kathmandu</li>
+                                                    <li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>+977 984 1215288</li>
+													<li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>+977 984 1215288</li>
+                                                    <li class="hover-text-primary"><i class="fas fa-envelope text-white mr-2 font-13 mt-1"></i>dippes10@gmail.com</li>
+                                                </ul>
+                                            </div>
+                                            <div class="footer-widget media-widget mt-4 text-white hover-text-primary"> <a href="#"><i class="fab fa-facebook-f"></i></a> <a href="#"><i class="fab fa-twitter"></i></a> <a href="#"><i class="fab fa-google-plus-g"></i></a> <a href="#"><i class="fab fa-linkedin-in"></i></a>  </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row copyright">
+                    <div class="col-sm-6"> <span class="text-white">© <?php echo date('Y');?> Home Link- Developed By Dipesh</span> </div>
+                    <div class="col-sm-6">
+                        <ul class="line-menu text-white hover-text-primary float-right">
+                            <li><a href="#"> Site Map</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
